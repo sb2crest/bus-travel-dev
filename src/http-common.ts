b5e0fc33a72df4b1cfb5f1946a1ba6129028de84
seabed2crest <courses@seabed2crest.com>
2023-10-05 09:08:40 +0530

    Vite Env implementation

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8085/",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-type": "application/json"
   }
