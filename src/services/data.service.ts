9a9aa15f241a0ae58554110c5ca42453ad37743b
seabed2crest <courses@seabed2crest.com>
2023-11-09 09:39:57 +0530

    build issue resolved

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -2,12 +2,13 @@ import http from "../http-common";
 import IOTPData from "../types/otp.type";
 import IVehicleData from "../types/vehicle.type";
 import ListVehicle from "../types/list.type";
-import IBookingInfoRequest from "../types/bookinginfo/request.type";
-import IBookNowRequest from "../types/booknow/request.type";
 import IPaymentRequest from "../types/payment/payment.request.type";
 import IGetInTouch from "../types/getInTouch.type";
 import IVerifyPayment from "../types/payment/VerifyPayment.type";
 import IFilterRequest from "../types/filter/request.type";
+import IBookNowRequest from "../types/BookNow/request.type";
+import IBookingInfoRequest from "../types/BookingInfo/request.type";
+// import IBookingInfoRequest from "../types/BookingInfo/request.type";
 class DataService {
 
   // Book now
