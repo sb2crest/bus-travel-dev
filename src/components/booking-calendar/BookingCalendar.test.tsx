8119b5e02acdb0ee764946cfa7edba2c431fcebc
sb2crest <courses@seabed2crest.com>
2023-11-27 18:14:46 +0530

    completed test cases for Bookingcalender

--- src/components/booking-calendar/BookingCalendar.test.tsx
+++ src/components/booking-calendar/BookingCalendar.test.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import BookingCalendar from "./BookingCalendar";
-
 
 describe("Booking Calendar component", () => {
   test("renders without errors", () => {
@@ -13,4 +12,27 @@ describe("Booking Calendar component", () => {
     );
 
    });
+   test("selects start date", () => {
+    render(
+      <MemoryRouter>
+        <BookingCalendar />
+      </MemoryRouter>
+    );
+    fireEvent.click(screen.getByPlaceholderText("From-Date"));
+    const startDate = screen.getByLabelText("Choose Wednesday, November 1st, 2023"); 
+    fireEvent.click(startDate);
+    expect((screen.getByPlaceholderText("From-Date") as HTMLInputElement).value).toContain("1");
   });
+  test("selects end date", () => {
+    render(
+      <MemoryRouter>
+        <BookingCalendar />
+      </MemoryRouter>
+    );
+    fireEvent.click(screen.getByPlaceholderText("To-Date"));
+    const endDate = screen.getByText("10") as HTMLElement; 
+    fireEvent.click(endDate);
+    expect((screen.getByPlaceholderText("To-Date") as HTMLInputElement).value).toContain("10");
+  });
+
+});
