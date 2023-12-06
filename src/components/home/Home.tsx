e2c57ad41f278b9d3dd61aa560faf1672caf167a
sb2crest <courses@seabed2crest.com>
2023-12-06 10:01:23 +0530

    Home component button bug resolved

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -158,9 +158,9 @@ const Home: React.FC = () => {
                   </p>
                 </div>
               </div>
-              <Link to={"/aboutus"} onClick={scrollToTop}>
                 {" "}
                 <div className="btn-container">
+              <Link to={"/aboutus"} onClick={scrollToTop}>
                   <button className="Knowmore">
                     <span className="text">Know more</span>
                     <div className="icon_container">
@@ -176,13 +176,14 @@ const Home: React.FC = () => {
                       </div>
                     </div>
                   </button>
+              </Link>
+
                 </div>
                 <svg style={{ display: "none" }}>
                   <symbol id="arrow-right" viewBox="0 0 20 10">
                     <path d="M14.84 0l-1.08 1.06 3.3 3.2H0v1.49h17.05l-3.3 3.2L14.84 10 20 5l-5.16-5z"></path>
                   </symbol>
                 </svg>
-              </Link>
             </div>
           </Slide>
           <Slide right>
