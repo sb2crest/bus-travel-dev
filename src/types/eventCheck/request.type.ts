3358bacbac7529eccd43cdbe4de5a67b7ef85580
sb2crest <courses@seabed2crest.com>
2024-01-24 18:29:23 +0530

    deleted unwanted components

--- src/types/eventCheck/request.type.ts
+++
@@ -1,9 +0,0 @@
-export default interface IEventTypeRequest {
-  source: string;
-  destination: string | null;
-  sourceLatitude: number;
-  sourceLongitude: number;
-  destinationLatitude: number;
-  destinationLongitude: number;
-  multipleDestination: boolean;
-}
